6f8ce267c4e8d91cf2a029ede7b98682995e2fcb
Siddhant Misra <msiddhant22@gmail.com>
2019-06-03 20:27:21 -0400

    Create 2Sum.java

---
+++ 2Sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (map.containsKey(complement)) {
+            return new int[] { map.get(complement), i };
+        }
+        map.put(nums[i], i);
+    }
+    throw new IllegalArgumentException("No two sum solution");
+   }
+}
+
+//we create a hashmap with the values and subtract the target from each of the numbers 
+//then we search if the complement exists in the hashmap and return the indices
